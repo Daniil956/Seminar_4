ef429a8bfdc3a12c560e03f66a2d7cb83839e8c1
Даниил <geimer_cok@mail.ru>
2022-12-01 21:42:14 +0300

    Пробуем

---
+++ Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Решение в группах задач:
+// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+Console.WriteLine("Enter number:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int lng = number.length;
+
+console.WriteLine(lng);
